5bc7d9c4a01e1ebffa433bf1022a9ee185573ea0
anjuchawla <anju.chawla@georgiancollege.ca>
2017-09-14 12:18:09 -0400

    Complete Code

--- Lesson1_Thursday/frmWelcome.Designer.cs
+++ Lesson1_Thursday/frmWelcome.Designer.cs
@@ -28,30 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmWelcome));
             this.lblHeading = new System.Windows.Forms.Label();
             this.lblMessage = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
             this.btnEnglish = new System.Windows.Forms.Button();
             this.btnSpanish = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.printForm1 = new Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(this.components);
             this.SuspendLayout();
             // 
             // lblHeading
             // 
             this.lblHeading.AutoSize = true;
             this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHeading.Location = new System.Drawing.Point(229, 26);
+            this.lblHeading.Location = new System.Drawing.Point(245, 26);
             this.lblHeading.Name = "lblHeading";
             this.lblHeading.Size = new System.Drawing.Size(160, 25);
-            this.lblHeading.TabIndex = 0;
+            this.lblHeading.TabIndex = 5;
             this.lblHeading.Text = "Welcome to C#";
             // 
             // lblMessage
             // 
             this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblMessage.Location = new System.Drawing.Point(123, 86);
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblMessage.Location = new System.Drawing.Point(139, 86);
             this.lblMessage.Name = "lblMessage";
             this.lblMessage.Size = new System.Drawing.Size(373, 57);
-            this.lblMessage.TabIndex = 1;
+            this.lblMessage.TabIndex = 6;
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblName
             // 
@@ -60,33 +69,78 @@
             this.lblName.Location = new System.Drawing.Point(532, 354);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(96, 17);
-            this.lblName.TabIndex = 2;
+            this.lblName.TabIndex = 7;
             this.lblName.Text = "Anju Chawla";
             // 
             // btnEnglish
             // 
-            this.btnEnglish.Location = new System.Drawing.Point(106, 204);
+            this.btnEnglish.Location = new System.Drawing.Point(181, 204);
             this.btnEnglish.Name = "btnEnglish";
             this.btnEnglish.Size = new System.Drawing.Size(88, 35);
-            this.btnEnglish.TabIndex = 3;
+            this.btnEnglish.TabIndex = 0;
             this.btnEnglish.Text = "&English";
             this.btnEnglish.UseVisualStyleBackColor = true;
             this.btnEnglish.Click += new System.EventHandler(this.btnEnglish_Click);
             // 
             // btnSpanish
             // 
-            this.btnSpanish.Location = new System.Drawing.Point(457, 204);
+            this.btnSpanish.Location = new System.Drawing.Point(382, 204);
             this.btnSpanish.Name = "btnSpanish";
             this.btnSpanish.Size = new System.Drawing.Size(88, 35);
-            this.btnSpanish.TabIndex = 4;
+            this.btnSpanish.TabIndex = 1;
             this.btnSpanish.Text = "&Spanish";
             this.btnSpanish.UseVisualStyleBackColor = true;
+            this.btnSpanish.Click += new System.EventHandler(this.btnSpanish_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(139, 298);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 35);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(288, 298);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(88, 35);
+            this.btnPrint.TabIndex = 3;
+            this.btnPrint.Text = "&Print Form";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(437, 298);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(88, 35);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // printForm1
+            // 
+            this.printForm1.DocumentName = "document";
+            this.printForm1.Form = this;
+            this.printForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPreview;
+            this.printForm1.PrinterSettings = ((System.Drawing.Printing.PrinterSettings)(resources.GetObject("printForm1.PrinterSettings")));
+            this.printForm1.PrintFileName = null;
             // 
             // frmWelcome
             // 
+            this.AcceptButton = this.btnClear;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
             this.ClientSize = new System.Drawing.Size(651, 391);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnSpanish);
             this.Controls.Add(this.btnEnglish);
             this.Controls.Add(this.lblName);
@@ -109,6 +163,10 @@
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Button btnEnglish;
         private System.Windows.Forms.Button btnSpanish;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnExit;
+        private Microsoft.VisualBasic.PowerPacks.Printing.PrintForm printForm1;
     }
 }
 
--- Lesson1_Thursday/frmWelcome.cs
+++ Lesson1_Thursday/frmWelcome.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Name: Anju Chawla
+ * Date: September 14, 2017
+ * Purpose: This application displays the message 'Hello World' in English and  Spanish, depending on user choice. Output can be cleared and the form can be printed.
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +22,52 @@ namespace Lesson1_Thursday
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Print the message in English
+        /// </summary>
+        /// <param name="sender">The English button</param>
+        /// <param name="e">The event arguments</param>
         private void btnEnglish_Click(object sender, EventArgs e)
         {
+            lblMessage.Text = "Hello World!!";
 
+        }
+        /// <summary>
+        /// Print the message in Spanish
+        /// </summary>
+        /// <param name="sender">The spanish button</param>
+        /// <param name="e">The event arguments</param>
+        private void btnSpanish_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Hola Mundo!!";
+        }
+        /// <summary>
+        /// Clear the message
+        /// </summary>
+        /// <param name="sender">The Clear button</param>
+        /// <param name="e">The event arguments</param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = String.Empty; //""
+        }
+        /// <summary>
+        /// Close the application
+        /// </summary>
+        /// <param name="sender">The exit button</param>
+        /// <param name="e">The event arguments<</param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            Application.Exit();
+        }
+        /// <summary>
+        /// This will print the form
+        /// </summary>
+        /// <param name="sender">The Print Form button</param>
+        /// <param name="e">The event arguments<</param>
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            printForm1.Print();
         }
     }
 }
